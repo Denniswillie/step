f0ac8edb45ad0c5bf7caac58d7ecb682c5aae2d0
Dennis Willie <denniswillie2000@gmail.com>
2020-07-03 13:05:28 +0000

    Add typing feature on index.html

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,3 +16,45 @@ window.addEventListener("scroll", function(event){
         }
     });
 });
+
+const helloElement = document.querySelector(".typing");
+//add "typing" feature for h1 tag "Hello" on index.html page
+
+var i = 0;
+var text = "Hello";
+var timeOutTime = 50;
+var typer = true;
+var transition = true;
+
+function typing(){
+    if(i < text.length){
+        if(typer){
+            helloElement.innerHTML = "|";
+            typer = false;
+            setTimeout(typing, 500);
+        }
+        else{
+            if(transition){
+                helloElement.innerHTML = text.charAt(i);
+                transition = false;
+                i++;
+            }
+            else{
+                helloElement.innerHTML += text.charAt(i);
+                i++;
+            }
+            setTimeout(typing, timeOutTime);
+        }
+    }
+    else{
+        if(!typer){
+            helloElement.innerHTML += "|";
+            typer = true;
+        }
+        else{
+            helloElement.innerHTML = helloElement.innerText.slice(0,helloElement.innerText.length - 1);
+            typer = false;
+        }
+        setTimeout(typing, 400);
+    }
+};
