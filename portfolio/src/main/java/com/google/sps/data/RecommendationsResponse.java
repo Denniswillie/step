6a6aa8bde38d1e2db16a0f28f362598e21737044
Dennis Willie <denniswillie2000@gmail.com>
2020-07-13 14:00:19 +0000

    Add authentication so that user can only view and edit comments if the user is logged in. Add user email in the comment

--- portfolio/src/main/java/com/google/sps/data/RecommendationsResponse.java
+++ portfolio/src/main/java/com/google/sps/data/RecommendationsResponse.java
@@ -6,17 +6,29 @@ import java.util.List;
 public class RecommendationsResponse{
     private final List<Recommendation> recommendationsList;
     private final int maxNumberofRecommendationsDisplayed;
+    private final boolean isLoggedIn;
+    private final String url;
 
-    public RecommendationsResponse(List<Recommendation> recommendationsList, int maxNumberofRecommendationsDisplayed){
+    public RecommendationsResponse(List<Recommendation> recommendationsList, int maxNumberofRecommendationsDisplayed, boolean isLoggedIn, String url){
         this.recommendationsList = recommendationsList;
         this.maxNumberofRecommendationsDisplayed = maxNumberofRecommendationsDisplayed;
+        this.isLoggedIn = isLoggedIn;
+        this.url = url;
     }
 
     public List<Recommendation> getRecommendationsList(){
         return recommendationsList;
     }
 
-    public int maxNumberofRecommendationsDisplayed(){
+    public int getMaxNumberofRecommendationsDisplayed(){
         return maxNumberofRecommendationsDisplayed;
+    }
+
+    public boolean isLoggedIn(){
+        return isLoggedIn;
+    }
+
+    public String getURL(){
+        return url;
     }
 }
