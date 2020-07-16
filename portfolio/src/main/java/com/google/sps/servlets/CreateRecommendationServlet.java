94b9d719a9be1c495753a321e4d1f83a2396a447
Dennis Willie <denniswillie2000@gmail.com>
2020-07-16 13:26:54 +0000

    Add login prompt so if the web page loads slowly and the user is able to create recommendation without logging in, then the user will be prompted to a login page to enter the user's google account

--- portfolio/src/main/java/com/google/sps/servlets/CreateRecommendationServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/CreateRecommendationServlet.java
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps.servlets;
 
 import com.google.appengine.api.datastore.Key;
@@ -40,23 +54,31 @@ public class CreateRecommendationServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    if(userService.isUserLoggedIn()){
+        String name = request.getParameter("name");
+        String relationship = request.getParameter("relationship");
+        String comment = request.getParameter("comment");
+        String email = userService.getCurrentUser().getEmail();
+        long timestamp = System.currentTimeMillis();
+
+        Entity recommendationEntity = new Entity("Recommendation");
+        recommendationEntity.setProperty("name", name);
+        recommendationEntity.setProperty("relationship", relationship);
+        recommendationEntity.setProperty("comment", comment);
+        recommendationEntity.setProperty("email", email);
+        recommendationEntity.setProperty("timestamp", timestamp);
+
+        dataStoreService.put(recommendationEntity);
+
+        response.sendRedirect("/recommendations.html");
+    }
+
+    else{
+        //user will be prompted to login
+        response.sendRedirect(userService.createLoginURL("/recommendations.html"));
+    }
     
-    String name = request.getParameter("name");
-    String relationship = request.getParameter("relationship");
-    String comment = request.getParameter("comment");
-    String email = userService.getCurrentUser().getEmail();
-    long timestamp = System.currentTimeMillis();
-
-    Entity recommendationEntity = new Entity("Recommendation");
-    recommendationEntity.setProperty("name", name);
-    recommendationEntity.setProperty("relationship", relationship);
-    recommendationEntity.setProperty("comment", comment);
-    recommendationEntity.setProperty("email", email);
-    recommendationEntity.setProperty("timestamp", timestamp);
-
-    dataStoreService.put(recommendationEntity);
-
-    response.sendRedirect("/recommendations.html");
   }
 }
 
