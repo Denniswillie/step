fd97e54a2464a8bf7a376bc03d4e54e7e1e1a258
Dennis Willie <denniswillie2000@gmail.com>
2020-08-03 02:20:54 +0000

    Fix spaces

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -25,30 +25,7 @@ import java.util.LinkedList;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
-* To be deleted after the code is finished.
-* 
-* Algorithm workflow:
-*   1.  Populating mandatoryTimeRanges (The available TimeRanges for all mandatory attendees) => Sort the collection 
-*       of events based on the events' timeranges start time. Once the collection is sorted, iterate through the events 
-*       and only take action when an event contains one or more mandatory attendees. The way it works is by using
-*       a TimeRange window. The window represents the longest filled TimeRange after the last free TimeRange. At the start
-*       of the iteration, the TimeRange window represents the start of day (i.e. the point at 00:00). If an event that is 
-*       currently being iterated through, starts after (not at) the end of the TimeRange window, then the event will be the next 
-*       TimeRange window and the gap between the previous TimeRange window and the current TimeRange window will be considered
-*       to be added to the mandatoryTimeRanges.
-*
-*   2.  Populating optionalTimeRanges (The unavailable TimeRanges for all optional attendees) => While iterating through the events
-*       for populating mandatoryTimeRanges, the events that don't have any mandatory attendees but have one or more optional attendees
-*       will be added to this collection. But before adding it to the collection, the algorithm will see if the event starts after
-*       (not at) the last event on the collection, if not, then the event will be merged with the last event on the collection.
-*
-*   3.  Populating mixedTimeRanges (The available TimeRanges for all mandatory and optional attendees) => 
-*/
-
-/**
-* FindMeetingQuery is the container class for the public method query.
-*/
+/** FindMeetingQuery is the container class for the public method query. */
 public final class FindMeetingQuery {
 
     /**
@@ -63,74 +40,82 @@ public final class FindMeetingQuery {
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
         List<Event> eventsList = new ArrayList(events);
 
-        //EventsList must be sorted so the three TimeRanges can be populated starting from the beginning to end of day.
+        // EventsList must be sorted so the three TimeRanges can be populated 
+        // starting from the beginning to end of day.
         Collections.sort(eventsList, Event.ORDER_BY_TIMERANGE_START_TIME);
 
-        //AVAILABLE time slots for mandatory attendees
+        // AVAILABLE time slots for mandatory attendees
         List<TimeRange> mandatoryTimeRanges = getMandatoryTimeRanges(request, eventsList);
       
-        //UNAVAILABLE time slots for optional attendees
+        // UNAVAILABLE time slots for optional attendees
         List<TimeRange> optionalTimeRanges = getOptionalTimeRanges(request, eventsList);
 
-        if(mandatoryTimeRanges.isEmpty() || optionalTimeRanges.isEmpty()){
+        if (mandatoryTimeRanges.isEmpty() || optionalTimeRanges.isEmpty()) {
             return mandatoryTimeRanges;
         }            
 
-        //AVAILABLE time slots that accommodate all mandatory and optional attendees
-        List<TimeRange> mixedTimeRanges = getMixedTimeRanges(mandatoryTimeRanges, optionalTimeRanges, request);
+        // AVAILABLE time slots that accommodate all mandatory and optional attendees
+        List<TimeRange> mixedTimeRanges = getMixedTimeRanges(mandatoryTimeRanges, 
+                                                            optionalTimeRanges, request);
 
         return (mixedTimeRanges.isEmpty()) ? mandatoryTimeRanges : mixedTimeRanges;
     }
 
-    private List<TimeRange> getMandatoryTimeRanges(MeetingRequest request, List<Event> eventsList){
+    /**
+    * Iterate through the eventsList to see if there's any intersections between the event's attendees and 
+    * mandatory attendees. This method uses {@code window} to mark the longest filled {@code TimeRange} after
+    * the last free {@code TimeRange}. At the start of the iteration, {@code window} represents the start of
+    * day. 
+    */
+    private List<TimeRange> getMandatoryTimeRanges(MeetingRequest request, List<Event> eventsList) {
         List<TimeRange> mandatoryTimeRanges = new ArrayList<>();
         Set<String> mandatoryAttendees = new HashSet<>(request.getAttendees());
         TimeRange window = TimeRange.fromStartDuration(0,0);
-        for(Event event: eventsList){
+        for (Event event: eventsList) {
             Set<String> mandatoryIntersectionSet = new HashSet<>();
             Set<String> eventAttendees = event.getAttendees();
             SetView<String> intersection = Sets.intersection(mandatoryAttendees, eventAttendees);
             mandatoryIntersectionSet = intersection.copyInto(mandatoryIntersectionSet);
-            if(!mandatoryIntersectionSet.isEmpty()){
-                if(event.getWhen().start() <= window.end()){
+            if (!mandatoryIntersectionSet.isEmpty()) {
+                if (event.getWhen().start() <= window.end()) {
                     int newStartTime = Math.min(event.getWhen().start(), window.start());
                     int newEndTime = Math.max(event.getWhen().end(), window.end());
                     window = TimeRange.fromStartEnd(newStartTime, newEndTime, false);
                 }
-                else{
-                    if(event.getWhen().start() - window.end() >= request.getDuration()){
+                else {
+                    if (event.getWhen().start() - window.end() >= request.getDuration()) {
                         mandatoryTimeRanges.add(TimeRange.fromStartEnd(window.end(), event.getWhen().start(), false));
                     }
                     window = TimeRange.fromStartEnd(event.getWhen().start(), event.getWhen().end(), false);
                 }
             }
         }
-        if(TimeRange.END_OF_DAY + 1 - window.end() >= request.getDuration()){
+        if (TimeRange.END_OF_DAY + 1 - window.end() >= request.getDuration()) {
             mandatoryTimeRanges.add(TimeRange.fromStartEnd(window.end(), TimeRange.END_OF_DAY, true));
         }
         return Collections.unmodifiableList(mandatoryTimeRanges);
     }
 
-    private List<TimeRange> getOptionalTimeRanges(MeetingRequest request, List<Event> eventsList){
+    private List<TimeRange> getOptionalTimeRanges(MeetingRequest request, List<Event> eventsList) {
         List<TimeRange> optionalTimeRanges = new LinkedList<>();
         Set<String> optionalAttendees = new HashSet<>(request.getOptionalAttendees());
-        for(Event event: eventsList){
+        for (Event event: eventsList) {
             int lastElement = optionalTimeRanges.size() - 1;
             Set<String> optionalIntersectionSet = new HashSet<>();
             Set<String> eventAttendees = event.getAttendees();
             SetView<String> intersection = Sets.intersection(eventAttendees, optionalAttendees);
             optionalIntersectionSet = intersection.copyInto(optionalIntersectionSet);
-            if(!optionalIntersectionSet.isEmpty()){
-                if(optionalTimeRanges.isEmpty()){
+            if (!optionalIntersectionSet.isEmpty()) {
+                if (optionalTimeRanges.isEmpty()) {
                     optionalTimeRanges.add(event.getWhen());
                 }
-                else if(event.getWhen().start() <= optionalTimeRanges.get(lastElement).end()){
+                else if (event.getWhen().start() <= optionalTimeRanges.get(lastElement).end()) {
                     int newStartTime = Math.min(event.getWhen().start(), optionalTimeRanges.get(lastElement).start());
                     int newEndTime = Math.max(event.getWhen().end(), optionalTimeRanges.get(lastElement).end());
                     optionalTimeRanges.remove(lastElement);
                     optionalTimeRanges.add(TimeRange.fromStartEnd(newStartTime, newEndTime, false));
                 }
-                else{
+                else {
                     optionalTimeRanges.add(event.getWhen());
                 }
             }
@@ -140,35 +125,32 @@ public final class FindMeetingQuery {
 
     private List<TimeRange> getMixedTimeRanges(List<TimeRange> mandatoryTimeRanges, 
                                                 List<TimeRange> optionalTimeRanges, 
-                                                MeetingRequest request){
-        if(optionalTimeRanges.get(optionalTimeRanges.size() - 1).end() < TimeRange.END_OF_DAY + 1){
+                                                MeetingRequest request) {
+        if (optionalTimeRanges.get(optionalTimeRanges.size() - 1).end() < TimeRange.END_OF_DAY + 1) {
             optionalTimeRanges.add(TimeRange.fromStartDuration(TimeRange.END_OF_DAY + 1, 0));
         }
         List<TimeRange> mixedTimeRanges = new ArrayList<>();
         TimeRange previousOptionalTimeRange = TimeRange.fromStartDuration(0, 0);
-        for(TimeRange optionalTimeRange: optionalTimeRanges){
+        for (TimeRange optionalTimeRange: optionalTimeRanges) {
             int gapStart = previousOptionalTimeRange.end();
             int gapEnd = optionalTimeRange.start();
             TimeRange currentGap = TimeRange.fromStartEnd(gapStart, gapEnd, false);
             previousOptionalTimeRange = optionalTimeRange;
-            for(TimeRange mandatoryTimeRange: mandatoryTimeRanges){
-                if(currentGap.contains(mandatoryTimeRange)){
+            for (TimeRange mandatoryTimeRange: mandatoryTimeRanges) {
+                if (currentGap.contains(mandatoryTimeRange)) {
                     mixedTimeRanges.add(mandatoryTimeRange);
                 }
-                else if(mandatoryTimeRange.contains(currentGap)){
-                    if(currentGap.duration() >= request.getDuration()){
-                        mixedTimeRanges.add(currentGap);
-                    }
+                else if(mandatoryTimeRange.contains(currentGap) 
+                        && currentGap.duration() >= request.getDuration()) {
+                    mixedTimeRanges.add(currentGap);
                 }
-                else if(currentGap.contains(mandatoryTimeRange.start())){
-                    if(currentGap.end() - mandatoryTimeRange.start() >= request.getDuration()){
-                        mixedTimeRanges.add(TimeRange.fromStartEnd(mandatoryTimeRange.start(), currentGap.end(), false));
-                    }
+                else if (currentGap.contains(mandatoryTimeRange.start()) 
+                        && currentGap.end() - mandatoryTimeRange.start() >= request.getDuration()) {
+                    mixedTimeRanges.add(TimeRange.fromStartEnd(mandatoryTimeRange.start(), currentGap.end(), false));
                 }
-                else if(currentGap.contains(mandatoryTimeRange.end())){
-                    if(mandatoryTimeRange.end() - currentGap.start() >= request.getDuration()){
-                        mixedTimeRanges.add(TimeRange.fromStartEnd(currentGap.start(), mandatoryTimeRange.end(), false));
-                    }
+                else if (currentGap.contains(mandatoryTimeRange.end()) 
+                        && mandatoryTimeRange.end() - currentGap.start() >= request.getDuration()) {
+                    mixedTimeRanges.add(TimeRange.fromStartEnd(currentGap.start(), mandatoryTimeRange.end(), false));
                 }
             }
         }
